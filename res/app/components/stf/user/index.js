554cc4953bc38abe520c418d2470efa4a294c73d
Maksim Alzhanov <m.alzhanov@vk.team>
2025-03-10 12:55:34 +0300

    rm -rf res

--- res/app/components/stf/user/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = angular.module('stf/user', [
-    require('stf/socket').name
-    , require('stf/common-ui').name
-    , require('stf/app-state').name
-])
-    .factory('UserService', require('./user-service'))
