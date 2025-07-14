1d58140c6181aa24dfc2092bb801460b1baacf4b
Elmir Khalilov <52529096+e-khalilov@users.noreply.github.com>
2025-07-14 19:41:49 +0300

    fix openid redirect (#276)

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -38,8 +38,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         authMock: resolve(__dirname, 'auth/auth-mock.html'),
-        authLdap: resolve(__dirname, 'auth/auth-ldap.html'),
-        authOpenid: resolve(__dirname, 'auth/auth-openid.html'),
+        authLdap: resolve(__dirname, 'auth/auth-ldap.html')
       },
     },
   },
