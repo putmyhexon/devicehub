40c51f7ef7996ef97d3df5f83b7bae3e252a7e3c
Daniil <8039921+DaniilSmirnov@users.noreply.github.com>
2025-09-08 16:23:24 +0500

    remove old icons (#356)
    * remove old icons
    
    * fix /lib lint

--- ui/src/constants/browser-icon-map.ts
+++ ui/src/constants/browser-icon-map.ts
@@ -1,34 +1,21 @@
 import softbankParentalControlsImage from '@/assets/browser-icons/softbank-parentalcontrols.png'
 import googleAndroidBrowserImage from '@/assets/browser-icons/google-android-browser.png'
-import samsungPopupbrowserImage from '@/assets/browser-icons/samsung-popupbrowser.png'
 import oppoBaiduSearchboxImage from '@/assets/browser-icons/oppo-baidu-searchbox.png'
 import netstarFamilysmileImage from '@/assets/browser-icons/netstar-familysmile.png'
 import htcSenseBrowserImage from '@/assets/browser-icons/htc-sense-browser.png'
-import operaMiniNativeImage from '@/assets/browser-icons/opera-mini-native.png'
-import samsungSbrowserImage from '@/assets/browser-icons/samsung-sbrowser.png'
 import fujitsuFbrowserImage from '@/assets/browser-icons/fujitsu-fbrowser.png'
 import androidBrowserImage from '@/assets/browser-icons/android-browser.png'
 import yahooYbrowserImage from '@/assets/browser-icons/yahoo-ybrowser.png'
 import lenovoBrowserImage from '@/assets/browser-icons/lenovo-browser.png'
-import chromeCanaryImage from '@/assets/browser-icons/chrome-canary.png'
 import firefoxBetaImage from '@/assets/browser-icons/firefox-beta.png'
 import asusBrowserImage from '@/assets/browser-icons/asus-browser.png'
-import chromeBetaImage from '@/assets/browser-icons/chrome-beta.png'
 import amazonSilkImage from '@/assets/browser-icons/amazon-silk.png'
 import yahooYjtopImage from '@/assets/browser-icons/yahoo-yjtop.png'
-import puffinFreeImage from '@/assets/browser-icons/puffin-free.png'
-import chromeDevImage from '@/assets/browser-icons/chrome-dev.png'
-import operaMiniImage from '@/assets/browser-icons/opera-mini.png'
-import operaBetaImage from '@/assets/browser-icons/opera-beta.png'
 import maxthonImage from '@/assets/browser-icons/maxthon.png'
 import firefoxImage from '@/assets/browser-icons/firefox.png'
 import exploreImage from '@/assets/browser-icons/explore.png'
-import chromeImage from '@/assets/browser-icons/chrome.png'
-import operaImage from '@/assets/browser-icons/opera.png'
 import baiduImage from '@/assets/browser-icons/baidu.png'
 import oneImage from '@/assets/browser-icons/one.png'
-import ucMiniImage from '@/assets/browser-icons/uc-mini.png'
-import ucImage from '@/assets/browser-icons/uc.png'
 
 export enum BrowserIcon {
   AMAZON_SILK = 'amazon-silk',
@@ -69,10 +56,11 @@ export const BROWSER_ICON_MAP: Record<string, string> = {
   [BrowserIcon.ANDROID_BROWSER]: androidBrowserImage,
   [BrowserIcon.ASUS_BROWSER]: asusBrowserImage,
   [BrowserIcon.BAIDU]: baiduImage,
-  [BrowserIcon.CHROME_BETA]: chromeBetaImage,
-  [BrowserIcon.CHROME_CANARY]: chromeCanaryImage,
-  [BrowserIcon.CHROME_DEV]: chromeDevImage,
-  [BrowserIcon.CHROME]: chromeImage,
+  [BrowserIcon.CHROME_BETA]: 'https://cdnjs.cloudflare.com/ajax/libs/browser-logos/75.0.1/chrome-beta/chrome-beta.svg',
+  [BrowserIcon.CHROME_CANARY]:
+    'https://cdnjs.cloudflare.com/ajax/libs/browser-logos/75.0.1/chrome-canary/chrome-canary.svg',
+  [BrowserIcon.CHROME_DEV]: 'https://cdnjs.cloudflare.com/ajax/libs/browser-logos/75.0.1/chrome-dev/chrome-dev.svg',
+  [BrowserIcon.CHROME]: 'https://cdnjs.cloudflare.com/ajax/libs/browser-logos/75.0.1/chrome/chrome.svg',
   [BrowserIcon.EXPLORE]: exploreImage,
   [BrowserIcon.FIREFOX_BETA]: firefoxBetaImage,
   [BrowserIcon.FIREFOX]: firefoxImage,
@@ -83,17 +71,20 @@ export const BROWSER_ICON_MAP: Record<string, string> = {
   [BrowserIcon.MAXTHON]: maxthonImage,
   [BrowserIcon.NETSTAR_FAMILYSMILE]: netstarFamilysmileImage,
   [BrowserIcon.ONE]: oneImage,
-  [BrowserIcon.OPERA_BETA]: operaBetaImage,
-  [BrowserIcon.OPERA_MINI_NATIVE]: operaMiniNativeImage,
-  [BrowserIcon.OPERA_MINI]: operaMiniImage,
-  [BrowserIcon.OPERA]: operaImage,
+  [BrowserIcon.OPERA_BETA]: 'https://cdnjs.cloudflare.com/ajax/libs/browser-logos/75.0.1/opera-beta/opera-beta.svg',
+  [BrowserIcon.OPERA_MINI_NATIVE]:
+    'https://cdnjs.cloudflare.com/ajax/libs/browser-logos/75.0.1/opera-mini/opera-mini_32x32',
+  [BrowserIcon.OPERA_MINI]: 'https://cdnjs.cloudflare.com/ajax/libs/browser-logos/75.0.1/opera-mini/opera-mini_32x32',
+  [BrowserIcon.OPERA]: 'https://cdnjs.cloudflare.com/ajax/libs/browser-logos/75.0.1/opera/opera.svg',
   [BrowserIcon.OPPO_BAIDU_SEARCHBOX]: oppoBaiduSearchboxImage,
-  [BrowserIcon.PUFFIN_FREE]: puffinFreeImage,
-  [BrowserIcon.SAMSUNG_POPUPBROWSER]: samsungPopupbrowserImage,
-  [BrowserIcon.SAMSUNG_SBROWSER]: samsungSbrowserImage,
+  [BrowserIcon.PUFFIN_FREE]: 'https://cdnjs.cloudflare.com/ajax/libs/browser-logos/75.0.1/puffin/puffin_32x32.png',
+  [BrowserIcon.SAMSUNG_POPUPBROWSER]:
+    'https://cdnjs.cloudflare.com/ajax/libs/browser-logos/75.0.1/samsung-internet/samsung-internet.svg',
+  [BrowserIcon.SAMSUNG_SBROWSER]:
+    'https://cdnjs.cloudflare.com/ajax/libs/browser-logos/75.0.1/samsung-internet/samsung-internet.svg',
   [BrowserIcon.SOFTBANK_PARENTALCONTROLS]: softbankParentalControlsImage,
-  [BrowserIcon.UC_MINI]: ucMiniImage,
-  [BrowserIcon.UC]: ucImage,
+  [BrowserIcon.UC_MINI]: 'https://cdnjs.cloudflare.com/ajax/libs/browser-logos/75.0.1/uc-mini/uc-mini_32x32.png',
+  [BrowserIcon.UC]: 'https://cdnjs.cloudflare.com/ajax/libs/browser-logos/75.0.1/uc/uc.svg',
   [BrowserIcon.YAHOO_YBROWSER]: yahooYbrowserImage,
   [BrowserIcon.YAHOO_YJTOP]: yahooYjtopImage,
 }
